b61646ae01b32367f523c85082a33fefb2c49c5a
Chaitanya-Kolwankar <chaitanyakolwankar@gmail.com>
2024-05-13 17:48:24 +0530

    first insert

--- booking.aspx.cs
+++ booking.aspx.cs
@@ -8,9 +8,11 @@ using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Data;
 
 public partial class booking : System.Web.UI.Page
 {
+    Class1 cls = new Class1();
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -46,7 +48,7 @@ public partial class booking : System.Web.UI.Page
             txt_email.Focus();
             return;
         }
-        else if(!isValid)
+        else if (!isValid)
         {
             ScriptManager.RegisterClientScriptBlock(this, typeof(Page), "anything", "$.notify('Invalid Email', { color: '#802019', background: '#ffb3b3', blur: 0.2, delay: 0 });", true);
             txt_email.Focus();
@@ -54,26 +56,32 @@ public partial class booking : System.Web.UI.Page
         }
         else
         {
+            DataTable dt = cls.fillDataTable("select [dbo].[GenerateIncrementedValue]()");
+            string Booking_id = dt.Rows[0][0].ToString().Trim();
+            string qyr = "insert into Turf_details (Booking_id,Name,Phone,Email,Turf_location,Turf_type,Turf_date,Form_time,To_time,Total_time,Total_amount,Adv_amount) values ('" + Booking_id + "','" + name + "'," + Phone + ",'" + Email + "','" + hidden_location.Value.Trim() + "','" + hidden_type.Value.Trim() + "','" + hidden_date.Value.Trim() + "','" + hidden_from_time.Value.Trim() + "','" + hidden_to_time.Value.Trim() + "','" + hidden_total_time.Value.Trim() + "'," + 100 + "," + 100 + ")";
+
+            cls.DMLqueries(qyr);
+
             ScriptManager.RegisterStartupScript(this, this.GetType(), "ReloadPage", "$('#Modal_qr').modal('show');", true);
-            string fromMail = "kolwankarchaitanya@gmail.com";
-            string fromPassword = "msrnfhxqkixszgmx";
+            //string fromMail = "kolwankarchaitanya@gmail.com";
+            //string fromPassword = "msrnfhxqkixszgmx";
 
-            MailMessage message = new MailMessage();
-            message.From = new MailAddress(fromMail);
-            message.Subject = "Test Subject";
-            message.To.Add(new MailAddress("kolwankarchaitanya@gmail.com"));
-            message.To.Add(new MailAddress(txt_email.Text.Trim()));
-            message.Body = "<html><body> Test Body </body></html>";
-            message.IsBodyHtml = true;
+            //MailMessage message = new MailMessage();
+            //message.From = new MailAddress(fromMail);
+            //message.Subject = "Test Subject";
+            //message.To.Add(new MailAddress("kolwankarchaitanya@gmail.com"));
+            //message.To.Add(new MailAddress(txt_email.Text.Trim()));
+            //message.Body = "<html><body> Test Body </body></html>";
+            //message.IsBodyHtml = true;
 
-            var smtpClient = new SmtpClient("smtp.gmail.com")
-            {
-                Port = 587,
-                Credentials = new NetworkCredential(fromMail, fromPassword),
-                EnableSsl = true,
-            };
+            //var smtpClient = new SmtpClient("smtp.gmail.com")
+            //{
+            //    Port = 587,
+            //    Credentials = new NetworkCredential(fromMail, fromPassword),
+            //    EnableSsl = true,
+            //};
 
-            smtpClient.Send(message);
+            //smtpClient.Send(message);
         }
     }
 }
